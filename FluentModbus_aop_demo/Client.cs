715e252e3d4073c042f7c2dcb6c0be46f1a10f24
黑内脏 <18736255770@163.com>
2022-03-31 18:59:22 +0800

    提交

--- FluentModbus_aop_demo/Client.cs
+++ FluentModbus_aop_demo/Client.cs
@@ -6,13 +6,12 @@ namespace FluentModbus_aop_demo
     {
         public string ReadCoils(string message)
         {
-
             /**
              * send request like
-             1byte	1byte	2bytes		2bytes		2bytes	
+             1byte	1byte	2bytes		2bytes		2bytes
              01	    06	    00	01	    00	32	    A8	3D
              * receive response like
-             1byte	1byte	2bytes		2bytes		2bytes	
+             1byte	1byte	2bytes		2bytes		2bytes
              01	    06	    00	01	    00	32	    A8	3D
              */
 
@@ -32,6 +31,4 @@ namespace FluentModbus_aop_demo
         public EventHandler<string> ResponseEventHandler { get; set; }
         public Func<string, string> ResponseHandler { get; set; }
     }
-
-
 }
